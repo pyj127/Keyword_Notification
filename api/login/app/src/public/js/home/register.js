07b9946db9618f8e51a4104d1b6b486507d037d4
julie0005 <duliesoures@gmail.com>
2021-02-05 23:18:19 +0900

    Sign up fetch complete

--- api/login/app/src/public/js/home/register.js
+++ api/login/app/src/public/js/home/register.js
@@ -2,18 +2,22 @@
 
 //질의선택자 : html에 있는 태그에서 하나를 선택하여 자바스크립트에서 제어할 수 있게 해줌. (DOM 객체)
 const id=document.querySelector("#id"), //태그에 id로 부여되어있는 "id"를 가져와라.
-psword=document.querySelector("#psword"),
-loginBtn=document.querySelector("#button");
+    name=document.querySelector("#name"),
+    psword=document.querySelector("#psword"),
+    confirmPsword=document.querySelector("#confirm-psword"),
+    registerBtn=document.querySelector("#button");
 
-loginBtn.addEventListener("click",login);
+registerBtn.addEventListener("click",register);
 
-function login(){
+function register(){
    const req = {
        id : id.value,
+       name : name.value,
        psword: psword.value,
+       confirmPsword : confirmPsword.value,
    };
-   
-   fetch("/login",{
+   console.log(req);
+   fetch("/register",{
        method: "POST",
        headers: {
         "Content-Type" : "application/json",
@@ -30,6 +34,6 @@ function login(){
 
         })
         .catch((err)=>{
-            console.error("로그인 중 에러 발생");
+            console.error("회원가입 중 에러 발생");
         });
 }
